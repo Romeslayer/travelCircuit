43c6ed4791f20382bd147b5328a158967c44ca9b
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-02 14:37:50 -0700

    Refactor to remove unnecessary variables

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,10 @@
 const getData = (url) => {
   return fetch(`http://localhost:3001/api/v1/${url}`)
      .then(response => response.json())
+     .then(data => data[url])
      .catch(e => console.log(e));
 }
 
-const travelersData = getData('travelers');
-const tripsData = getData('trips');
-const destinationsData = getData('destinations');
-
-
 export {
-  getData,
-  travelersData,
-  tripsData,
-  destinationsData
+  getData
 }
--- src/scripts.js
+++ src/scripts.js
@@ -4,10 +4,7 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/index.css';
 import {
-  getData,
-  travelersData,
-  tripsData,
-  destinationsData
+  getData
 } from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
