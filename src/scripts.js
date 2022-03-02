07d61b9ce0f3b4fcfb70b49fabd8360e7061629c
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-02 11:24:32 -0700

    Create API calls and import to scripts

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,19 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
+import {
+  getData,
+  travelersData,
+  tripsData,
+  destinationsData
+} from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
+const fetchData = () => {
+  Promise.all([getData('travelers'), getData('trips'), getData('destinations')]).then(data => {
+    console.log(data);
+  })
+}
+fetchData();
 console.log('This is the JavaScript entry file - your code begins here.');
