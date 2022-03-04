74213860a041890204aae6fb15190c2561085438
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-04 09:44:16 -0700

    Correct name of variable and file name

--- src/data/travelers-test-data.js
+++ src/data/travelers-test-data.js
@@ -1,4 +1,4 @@
-const travelerTestData = [
+const travelersTestData = [
   {id: 1, name: 'Ham Leadbeater', travelerType: 'relaxer'},
   {id: 2, name: 'Rachael Vaughten', travelerType: 'thrill-seeker'}
 ]
