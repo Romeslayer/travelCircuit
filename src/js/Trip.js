04670bb8541e2698d22b15fb8b140255bc4dc0f0
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-04 14:26:53 -0700

    Create class that pass tests

--- src/js/Trip.js
+++ src/js/Trip.js
@@ -1,5 +1,10 @@
 class Trip {
-  constructor(data) {
+  constructor(data, destination) {
+    Object.entries(data).forEach(prop => this[prop[0]] = prop[1]);
+    this.destination = destination;
+  }
+  getCost() {
+    return ((this.travelers * 2) * this.destination.estimatedFlightCostPerPerson) + (this.duration * this.destination.estimatedLodgingCostPerDay);
   }
 };
 
