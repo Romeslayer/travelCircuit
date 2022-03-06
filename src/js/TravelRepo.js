4afd8e51603b4d807a3189f29a53d307d01b00bf
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-06 13:13:55 -0700

    Create class to pass tests

--- src/js/TravelRepo.js
+++ src/js/TravelRepo.js
@@ -1,8 +1,27 @@
-class TravelRepo {
-  constructor(data) {
+import Destination from './Destination.js';
+import Trip from './Trip.js';
+import Traveler from './Traveler.js';
 
+class TravelRepo {
+  constructor(travelers, trips, destinations) {
+    this.destinations = destinations.map(des => new Destination(des));
+    this.trips = trips.map(trip => new Trip(trip, this.getDestination(trip.destinationID)));
+    this.travelers = travelers.map(traveler => new Traveler(traveler, this.getTrips(traveler.id)));
+    console.log(this.travelers);
   }
 
+  getDestination(id) {
+    return this.destinations.find(des => des.id === id) || 'Sorry that Destination id is invalid.';
+  }
+
+  getTrips(id) {
+    let trips = this.trips.filter(trip => trip.userID === id);
+    return trips.length ? trips : 'Sorry there are no Trips for this user id.';
+  }
+
+  getTraveler(id) {
+    return this.travelers.find(traveler => traveler.id === id) || 'Sorry there is no user with that id.';
+  }
 }
 
 export default TravelRepo;
