852ed5bb9f1ad2b8c333a802fdb805ed59e093fc
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-03-08 18:54:42 -0700

    Create login section and functionality to login and out

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,11 @@ import {
   drawTripCard,
   showResponse,
   updateEstimate,
-  resetEstimate
+  resetEstimate,
+  hideLogin,
+  showLogin,
+  showMain,
+  hideMain
 } from './dom-updates.js';
 import Traveler from './js/Traveler.js';
 import Destination from './js/Destination.js';
@@ -18,8 +22,10 @@ import TravelRepo from './js/TravelRepo.js';
 import Trip from './js/Trip.js';
 
 const todayDate = document.querySelector('#todayDate');
-let travel;
+const header = document.querySelector('header');
 
+let travel;
+let traveler;
 
 const fetchData = () => {
   Promise.all([getData('travelers'), getData('trips'), getData('destinations')]).then(data => {
@@ -27,17 +33,24 @@ const fetchData = () => {
   });
 }
 
-const getRandomTraveler = (travelRepo) => {
-  return travelRepo.getTraveler(Math.floor(Math.random() * 50) + 1)
+const fetchNewData = () => {
+  Promise.all([getData('travelers'), getData('trips'), getData('destinations')]).then(data => {
+    handleData(data);
+    loadUser(traveler.id);
+  });
 }
+
 
 const handleData = (data) => {
   let travelers = data[0];
   let trips = data[1];
   let destinations = data[2];
   travel = new TravelRepo(travelers, trips, destinations);
+}
+
+const loadUser = (id) => {
   let today =  new Date();
-  let traveler = getRandomTraveler(travel);
+  traveler = travel.getTraveler(id);
   updateTraveler(traveler, today)
   updateDay(today)
   setupForm(traveler, travel);
@@ -67,14 +80,54 @@ const submitForm = (e) => {
   e.preventDefault();
   tripIdInput.value = Date.now() + Math.random();
   if(checkForm()) {
-    postTrip(checkForm())
+    postTrip(checkForm());
+    resetEstimate();
     showResponse();
     e.target.reset();
-    fetchData();
+    fetchNewData()
   }
 }
 
+const validate = (data) => {
+  let username = data.get('username');
+  let password = data.get('password');
+  let valid = false;
+  const usernames = travel.travelers.map(traveler => `traveler${traveler.id}`);
+  const passwords = ['travel']
+
+  if(usernames.includes(username) && passwords.includes(password)) {
+    valid = true;
+  }
+  return valid;
+}
+
+const login = (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target)
+  if(validate(data)) {
+    message.innerText = `Login successful.`;
+    showResponse();
+    loadUser(parseInt(data.get('username').slice(8)));
+    hideLogin();
+    showMain();
+    e.target.reset();
+  } else {
+    message.innerText = `Username or password is incorrect.`;
+    showResponse();
+  }
+}
+
+const logout = (e) => {
+  if(e.target.id === 'logOut') {
+    message.innerText = `Log out successful`;
+    showResponse();
+    showLogin();
+    hideMain();
+  }
+}
 
 window.onload = fetchData;
 tripForm.onsubmit = submitForm;
 tripForm.oninput = checkForm;
+loginForm.onsubmit = login;
+header.onclick = logout;
